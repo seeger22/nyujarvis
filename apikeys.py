1c05db1a16f1a28eb4b8c23a4a153b61d5a0a809
Jiakai Zou <59547688+seeger22@users.noreply.github.com>
2023-12-15 01:47:28 -0500

    upload helper funcs, CharLLM license

---
+++ apikeys.py
@@ -0,0 +1,5 @@
+# Description: This file contains the API keys for the API calls to OpenAI gpt-3.5-turbo or gpt-4.
+apikey_list = [
+    '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',
+    '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'
+]
